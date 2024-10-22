b8e0a1a1b5714efda211764b56d3dfeae50a241d
Kasymova Elzada <elzada.kasymova2@gmail.com>
2024-10-22 15:41:00 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     "use strict";
+
+    // tabs---------------------------------------------------
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -58,4 +60,72 @@ document.addEventListener("DOMContentLoaded", () => {
     hideTabContent();
     showTabContent();
 
+
+    // timer--------------------------------------------------------
+
+    const deadline = '2024-11-24';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
+
+
+// Date 
+
+const newDate = document.querySelector('.promotion__descr');
+
+newDate.innerHTML = " ";
+
+newDate.innerHTML = `Мы ценим каждого клиента и предлагаем вам стать одним из них на очень выгодных условиях. 
+                    Каждому, кто закажет доставку питания на неделю, будет предоставлена скидка в размере <span>20%!</span>
+                    <br><br>
+                    Акция закончится 24 ноября в 00:00.`;
+
